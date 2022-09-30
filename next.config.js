073f169595c401b1e3142c10f50814507b2aeb5e
Skyler Vautier <98610597+SkyV0@users.noreply.github.com>
2022-09-30 11:29:44 -0400

    cors

--- next.config.js
+++ next.config.js
@@ -17,5 +17,18 @@ const nextConfig = {
     minimumCacheTTL: 9999999,
   },
 };
+    //set header first to allow request or origin domain (value can be different)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
-module.exports = withSuperjson()(withAnalyzer(nextConfig));
+//---- other code
+
+ //Preflight CORS handler
+    if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
+module.exports = withSuperjson()(req, res); withAnalyzer(nextConfig);
