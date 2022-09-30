9ef1b99055505cc99cd46fe2beeedff40a7ba81e
Skyler Vautier <98610597+SkyV0@users.noreply.github.com>
2022-09-30 11:31:03 -0400

    doesnt work

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withAnalyzer = require("@next/bundle-analyzer")({
   enabled:
-    process.env.ANALYZE === "true" && process.env.NODE_ENV !== "production",
+    process.env.ANALYZE === "true" && process.env.NODE_ENV !== "development",
 });
 const { withSuperjson } = require("next-superjson");
 
@@ -17,18 +17,5 @@ const nextConfig = {
     minimumCacheTTL: 9999999,
   },
 };
-    //set header first to allow request or origin domain (value can be different)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
-//---- other code
-
- //Preflight CORS handler
-    if(req.method === 'OPTIONS') {
-        return res.status(200).json(({
-            body: "OK"
-        }))
-    }
-module.exports = withSuperjson()(req, res); withAnalyzer(nextConfig);
+module.exports = withSuperjson()(withAnalyzer(nextConfig));
