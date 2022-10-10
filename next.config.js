865180605da8ff79ddf26810aa2017ceaee7ce1d
Skyler Vautier <98610597+SkyV0@users.noreply.github.com>
2022-10-10 14:50:34 -0400

    change to dev

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withAnalyzer = require("@next/bundle-analyzer")({
   enabled:
-    process.env.ANALYZE === "true" && process.env.NODE_ENV !== "production",
+    process.env.ANALYZE === "true" && process.env.NODE_ENV !== "development",
 });
 const { withSuperjson } = require("next-superjson");
 
